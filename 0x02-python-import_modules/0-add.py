9e379c7de7107fbacbf9522b8957e2aff2c75c9e
Franck Tchamo <francktchamo@yahoo.fr>
2022-06-02 03:11:53 -0700

    Correct 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,5 +4,6 @@ from add_0 import add
 
 
 if __name__ == '__main__':
-    print("{} + {} = {}".format(1, 2, add(1, 2)))
-    
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b))) 
