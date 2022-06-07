05e4609b7643f7e598dc0ab81c1b03361bf0b737
Franck Tchamo <francktchamo@yahoo.fr>
2022-06-07 07:26:59 -0700

    Correct is_palindrome function

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include "lists.h"
 
+//int size_listint(listint_t **head);
+//int *extract_int(listint_t **head);
 /**
  * is_palindrome - nction in C that checks
  * if a singly linked list is a palindrome.
@@ -24,4 +26,55 @@ int is_palindrome(listint_t **head)
 	}
 
 	return (1);
+}
+
+/**
+ * size_listint - gives the size of a listint_t list
+ * @head: pointer to pointer of the first node of listint_t list      
+ * Return: the size of the list or 0 if the list is NULL
+ */
+int size_listint(listint_t **head)
+{
+	listint_t *current;
+	int size;
+
+	size = 0;
+	current = *head;
+	while (current != NULL)
+	{
+		size++;
+		current = current->next;
+	}
+	return (size);
+}
+
+/**
+ * extract_int - retrieves every integer into a listsint_t list
+ * @head: pointer to pointer of the first node of listint_t list
+ * Return: a pointer to the adress of the list of int
+ */
+int *extract_int(listint_t **head)
+{
+	listint_t *current;
+	int size = size_listint(head);
+	int *mylist, i;
+
+	mylist = malloc(sizeof(int) * size);
+
+	if (mylist == NULL)
+	{
+		free(mylist);
+		return (NULL);
+	}
+
+	current = *head;
+	i = 0;
+	while (current != NULL)
+	{
+		mylist[i] = current->n;
+		i++;
+		current = current->next;
+	}
+
+	return (mylist);
 }
