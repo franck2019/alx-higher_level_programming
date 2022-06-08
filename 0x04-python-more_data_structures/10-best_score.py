868399d95f39193c7f025aeae6b1c5b62c3624b1
Franck Tchamo <francktchamo@yahoo.fr>
2022-06-08 16:31:08 -0700

    Add 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+
+    if a_dictionary is {} or a_dictionary is None:
+        return None
+
+    max_score_value = max(list(a_dictionary.values()))
+
+    for key, value in a_dictionary.items():
+        if value == max_score_value:
+            return key
