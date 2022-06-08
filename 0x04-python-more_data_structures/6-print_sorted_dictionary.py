1b316f83bc45a041e89438f194c998ae3edcc9bc
Franck Tchamo <francktchamo@yahoo.fr>
2022-06-08 11:03:05 -0700

    Remove space before the colon symbol(:)

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -6,4 +6,4 @@ def print_sorted_dictionary(a_dictionary):
     keys = sorted(list(a_dictionary.keys()))
 
     for key in keys:
-        print("{} : {}".format(key, a_dictionary[key]))
+        print("{}: {}".format(key, a_dictionary[key]))
