97ab673d6826926d51fa471db8a3b71f2f6a6156
Franck Tchamo <francktchamo@yahoo.fr>
2022-06-20 05:17:16 -0700

    Add 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python33
+
+
+def safe_print_list(my_list=[], x=0):
+
+    try:
+        counter = 0
+
+        for i in range(x):
+            print(my_list[i], end="")
+            counter += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+
+    return counter
