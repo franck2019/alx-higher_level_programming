bcb7ac7555f6f9ef765f441a5a6131a261fb64dd
Franck Tchamo <francktchamo@yahoo.fr>
2022-06-21 17:35:11 -0700

    Correct docstring and code style

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""0-square Module"""
+
+
 class Square:
-    """an empty class Square that defines a square"""
+    """an empty class Square that defines a square."""
     pass
