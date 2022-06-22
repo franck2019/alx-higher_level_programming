9bebafe613863eb776c8e107531d9658ad98bd49
Franck Tchamo <francktchamo@yahoo.fr>
2022-06-22 01:39:37 -0700

    Clean 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,17 +6,10 @@
 class Square:
     """a class Square that defines a square."""
 
-    def __init__(self, size=0):
+    def __init__(self, size):
         """The constructor of the square class
 
         Args:
            size (int): The size of a side of the square
         """
-
-        if(not isinstance(size, int)):
-            raise TypeError("The size of the Square must be an int")
-
-        if(size < 0):
-            raise ValueError("Size must greater or equal than zero")
-
         self.__size = size
