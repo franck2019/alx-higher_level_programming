517daddafe4518302cfcf079c48ccf81f70eb856
Franck Tchamo <francktchamo@yahoo.fr>
2022-06-23 09:58:51 -0700

    Correct code style 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,24 +3,27 @@ import math
 
 """The magic Module."""
 
+
 class MagicClass:
-	
-	def __init__(self, radius):
-		
-		self.__radius = 0
-		if type(radius) is not int and type(radius) is not float:
-			raise TypeError("radius must a number")
-		
-		self.__radius = radius
 
-	def area(self):
-		return (self.__radius ** 2) * math.pi
-		
-	def circumference(self):
-		return 2 * math.pi * self.__radius
+    def __init__(self, radius):
+
+        self.__radius = 0
+
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must a number")
+        self.__radius = radius
+
+    def area(self):
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        return 2 * math.pi * self.__radius
 
 
+"""
 import dis
 
 
 dis.dis(MagicClass)
+"""
