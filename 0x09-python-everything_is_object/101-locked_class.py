992783e6288421c6d443533142555ab35abc9dcc
Franck Tchamo <francktchamo@yahoo.fr>
2022-06-29 09:25:46 -0700

    Add 101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module has a class called LockedClass."""
+
+
+class LockedClass:
+    """The LockedClass."""
+
+    def __setattr__(self, attr_name, attr_value):
+        """Prevents the user from dynamically creating
+        new instance attributes except if the new instance
+        attribute is called <first_name>.
+
+        Args:
+            attr_name: the name of the attribute
+            attr_value: the value of the attribute."""
+
+        if attr_name != "first_name":
+            sms = "'LockedClass' object has no attribute '{}'"
+            raise AttributeError(sms.format(attr_name))
+
+        self.__dict__[attr_name] = attr_value
