7848c092c2ff2fd0cbac96533bfbe8c4f2877b99
Franck Tchamo <francktchamo@yahoo.fr>
2022-07-03 06:58:23 -0700

    Correct1 0-add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,7 +11,7 @@ def add_integer(a, b=98):
     Args: a (int): first argument.
           b (int): second argument."""
 
-    if type(a) not in [int, float]:
+    if a is None or type(a) not in [int, float]:
         raise TypeError('a must be an integer')
 
     if type(b) not in [int, float]:
