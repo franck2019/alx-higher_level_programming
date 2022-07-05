850654c6935c9b2287d533088b11e4683cedccbf
Franck Tchamo <francktchamo@yahoo.fr>
2022-07-04 22:52:13 -0700

    Add 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""same_object module"""
+
+
+def is_same_class(obj, a_class):
+    """function that returns True if the object is exactly
+    an instance of the specified class ; otherwise False
+
+    Args:
+        obj (:obj): an object
+        a_class: a class
+    """
+    return isinstance(obj, a_class)
