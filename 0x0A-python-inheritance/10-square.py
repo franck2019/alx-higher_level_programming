6eedf47827d501227e3c7a2c6037829f244a2a5a
Franck Tchamo <francktchamo@yahoo.fr>
2022-07-08 17:45:03 -0700

    Correct1 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -16,6 +16,7 @@ class Square(Rectangle):
         Return:
             Always nothing
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
