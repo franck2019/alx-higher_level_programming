ec56e3bdd0ed70a81cbd6f7faa482d56e7fcf365
Franck Tchamo <francktchamo@yahoo.fr>
2022-07-12 16:21:50 -0700

    Correct 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,13 +2,22 @@
 """add_item module"""
 
 import sys
-import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = 'add_item.json'
 
+try:
+    old_content = load_from_json_file(filename)
+except Exception:
+    save_to_json_file([], filename)
+else:
+    args = sys.argv[1:]
+    old_content.extend(args)
+    save_to_json_file(old_content, filename)
+
+"""
 if len(sys.argv) <= 1:
     save_to_json_file([], filename)
 else:
@@ -18,3 +27,4 @@ else:
     save_to_json_file(old_content, filename)
 
 load_from_json_file(filename)
+"""
