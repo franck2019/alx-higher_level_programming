6abad5a83b7cc3c5b335e1a672aa777550c1dc33
Franck Tchamo <francktchamo@yahoo.fr>
2022-07-15 06:04:34 -0700

    Add 101-stats.py

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""stats module: script that reads stdin line by line and computes metrics."""
+
+import sys
+import os
+
+
+def stat_1(a_dict):
+
+    total = 0
+
+    for code, value in a_dict.items():
+        total += value
+
+    print(f"File size: {total}")
+
+
+def stat_2(a_dict):
+
+    for key, value in a_dict.items():
+        print(f"{key}: {value}")
+
+
+status_file = dict()
+status_ocurrence = dict()
+
+counter_10 = 0
+
+try:
+    while True:
+
+        line = sys.stdin.readline()
+        line_1 = tuple(line.split()[-2:])
+        status_code, file_size = line_1
+        # print(status_code, file_size)
+
+        if status_code not in status_file:
+            status_file[status_code] = int(file_size)
+            status_ocurrence[status_code] = 1
+        else:
+            status_file[status_code] += int(file_size)
+            status_ocurrence[status_code] += 1
+
+        counter_10 += 1  # increment the counter
+
+        if counter_10 == 10:
+            stat_1(status_file)
+            stat_2(status_ocurrence)
+            counter_10 = 0
+
+except KeyboardInterrupt as ki:
+    stat_1(status_file)
+    stat_2(status_ocurrence)
+    print(ki)
