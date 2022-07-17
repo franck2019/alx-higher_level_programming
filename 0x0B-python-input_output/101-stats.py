353eb344313ea5cd3da2c215ced1efd1c7f52239
Franck Tchamo <francktchamo@yahoo.fr>
2022-07-17 13:41:25 -0700

    Correct1 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,7 +2,6 @@
 """stats module: script that reads stdin line by line and computes metrics."""
 
 import sys
-import os
 
 
 def stat_1(a_dict):
@@ -25,15 +24,13 @@ def stat_2(a_dict):
 status_file = {}
 status_ocurrence = {}
 
-counter_10 = 0
+counter = 0
 
 try:
     while True:
-
         line = sys.stdin.readline()
         line_1 = tuple(line.split()[-2:])
         status_code, file_size = line_1
-        # print(status_code, file_size)
 
         if status_code not in status_file:
             status_file[status_code] = int(file_size)
@@ -42,13 +39,12 @@ try:
             status_file[status_code] += int(file_size)
             status_ocurrence[status_code] += 1
 
-        counter_10 += 1  # increment the counter
+        counter += 1  # increment the counter
 
-        if counter_10 == 10:
+        if counter == 10:
             stat_1(status_file)
             stat_2(status_ocurrence)
-            counter_10 = 0
-
+            counter = 0
 except KeyboardInterrupt as ki:
     stat_1(status_file)
     stat_2(status_ocurrence)
