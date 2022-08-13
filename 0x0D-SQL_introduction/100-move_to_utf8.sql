b4ef3a3d47d7c1fe71b49456a0b4c56cdc956717
Franck Tchamo <francktchamo@yahoo.fr>
2022-08-12 20:49:50 -0700

    Correct 2

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,10 +3,13 @@
 USE hbtn_0c_0;
 
 ALTER DATABASE hbtn_0c_0 
-	CHARACTER SET ="utf8mb4"
-	COLLATE ="utf8mb4_unicode_ci";
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
 
--- convert field name
 ALTER TABLE first_table
-	CHANGE name name VARCHAR(256)
-    	COLLATE utf8mb4_unicode_ci;
+	CHARACTER SET utf8mb4 
+	COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table MODIFY
+	name VARCHAR(256)
+		COLLATE utf8mb4_unicode_ci;
