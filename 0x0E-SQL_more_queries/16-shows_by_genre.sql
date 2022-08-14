a93bac85c712c7a5f8779428cf6e1d1c1b887b99
Franck Tchamo <francktchamo@yahoo.fr>
2022-08-13 17:13:45 -0700

    Add 16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,18 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: 
+-- download (same as 15-count_shows_by_genre.sql)
+
+-- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+
+-- If a show doesn’t have a genre, display NULL in the genre column
+-- Each record should display: tv_shows.title - tv_genres.name
+-- Results must be sorted in ascending order by the show title and genre name
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT s.title, g.name
+FROM tv_shows s
+LEFT JOIN tv_show_genres sg
+ON s.id = sg.show_id
+LEFT JOIN tv_genres g
+ON sg.genre_id = g.id
+ORDER BY s.title;
+
